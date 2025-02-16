7028e3c28bfc1a9bb18abb10955ce5e34d241577
mahlen <malink027@gmail.com>
2025-02-17 00:36:33 +1100

    updated styling for mobile for account api page

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -3,7 +3,7 @@ import ConnectAPI from "../components/personal-assistant/ConnectAPI";
 export default function AccountPage() {
   return (
     <div className="bg-[#305555]">
-      <div className="flex justify-center items-start p-24 min-h-screen">
+      <div className="flex justify-center items-start px-2 md:px-24 py-24 min-h-screen">
         <ConnectAPI />
       </div>
     </div>
