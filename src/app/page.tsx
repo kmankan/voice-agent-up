77778fb11bd757d2c291b5bdd59ee40c2c8fc225
mahlen <malink027@gmail.com>
2025-02-17 22:27:42 +1100

    mobile styling adjustments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,11 @@ import { AccountExplorerCard } from './components/main/AccountExplorerCard';
 export default function Home() {
   return (
     <main className="min-h-screen p-8 bg-[#ff705c]">
-      <div className="max-w-4xl mx-auto space-y-16">
+      <div className="max-w-4xl mx-auto space-y-8">
         <h1 className="text-5xl font-extrabold text-[#ffee52] text-center font-circular">
           Up Bank Assistant
         </h1>
-        <div className="flex flex-col gap-12 max-w-xl mx-auto">
+        <div className="flex flex-col gap-8 max-w-xl mx-auto">
           <UpDashboardCard />
           <AccountExplorerCard />
         </div>
